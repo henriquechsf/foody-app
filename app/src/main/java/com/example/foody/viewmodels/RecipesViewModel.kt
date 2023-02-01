b5490691634d35e1fb44d0de672ee7951580cefa
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-01-31 21:26:11 -0300

    feat: create and inject DataStoreRepository class

--- app/src/main/java/com/example/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/foody/viewmodels/RecipesViewModel.kt
@@ -2,8 +2,10 @@ package com.example.foody.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import com.example.foody.utils.Constants
 import com.example.foody.utils.Constants.Companion.API_KEY
+import com.example.foody.utils.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.foody.utils.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.example.foody.utils.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import com.example.foody.utils.Constants.Companion.QUERY_ADD_RECIPE_INFO
 import com.example.foody.utils.Constants.Companion.QUERY_API_KEY
 import com.example.foody.utils.Constants.Companion.QUERY_DIET
@@ -16,10 +18,10 @@ class RecipesViewModel(application: Application) : AndroidViewModel(application)
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_NUMBER] = "50"
-        queries[QUERY_TYPE] = "snack"
-        queries[QUERY_DIET] = "vegan"
+        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
         queries[QUERY_ADD_RECIPE_INFO] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
